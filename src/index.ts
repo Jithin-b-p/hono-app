eb99f1b349eb6c5ab323a571205ab2d226b385ad
unknown <jithinbp007@gmail.com>
2024-07-21 23:02:48 +0530

    methods of context object

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,12 @@ import { Hono } from "hono";
 
 const app = new Hono();
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
+app.get("/welcome", (c) => {
+  // return c.body("this is a text message", 200, {
+  //   "Content-Type": "text/plain",
+  // });
+
+  return c.json({ message: "This is a json message" }, 200);
 });
 
 export default app;
